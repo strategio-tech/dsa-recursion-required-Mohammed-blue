9321cc63ebd558ffb46a9cd3f2fbca9490e2859f
Mohammed Mansour <mohammed0302093@gmail.com>
2023-01-14 19:28:23 -0500

    resolved

--- src/main/java/tech/strategio/E01.java
+++ src/main/java/tech/strategio/E01.java
@@ -3,8 +3,17 @@ package tech.strategio;
 import java.util.*;
 
 public class E01 {
-    public int getNthFib(int n) {
-        // FIXME
-        return -1;
+    public static int getNthFib(int n) {
+        if (n==0||n==1)
+        return 0;
+        else if(n==2)
+            return 1;
+        return getNthFib(n - 1) + getNthFib(n - 2);
+    }
+
+    public static void main(String args[])
+    {
+        int n = 9;
+        System.out.println(getNthFib(n));
     }
 }
